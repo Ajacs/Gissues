8b024b13a2f75517d327af3f93118f0a5105bba4
adderlyjc <ajacs1104@gmail.com>
2018-05-05 17:25:54 -0500

    Added search endpoint to the endpoint object, show/hidde buttons in the wizard depdending the currentstep and searchBy value.

--- src/components/commons/wizard/wizardFooter/wizardFooter.js
+++ src/components/commons/wizard/wizardFooter/wizardFooter.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { Button, Grid } from 'semantic-ui-react';
+import { SEARCH_BY } from 'constants/searchTypes';
+import classNames from 'classnames';
 
-const WizardFooter = ({actions}) => (
-  <Grid.Row style={{ padding: '10px', display: 'inline-block'}}>
-    <Button
-    onClick={actions.onCancelClicked}
-    color="red"
-    size='medium'
-    floated="left">
-      Cancelar
-    </Button>
-    <Button.Group floated="right" size='medium' >
-      <Button onClick={actions.onPreviousClicked}>Anterior</Button>
-      <Button.Or />
-      <Button color="blue" onClick={actions.onNextClicked}>Siguiente</Button>
-    </Button.Group >
-  </Grid.Row >
-);
+const WizardFooter = ({actions, searchBy, currentStep}) => {
+    let visibleOrBackButtons = true;
+    let visibleNextButton = true;
+
+    if ( currentStep === 2 && searchBy === SEARCH_BY.REPOSITORIES) {
+        visibleOrBackButtons = false;
+    } else if(currentStep === 1 && searchBy === SEARCH_BY.USERS) {
+        visibleOrBackButtons = false;
+    } else if( currentStep === 3) {
+        visibleNextButton = false;
+    }
+
+    return (
+        <Grid.Row style={{ padding: '10px', display: 'inline-block'}}>
+            <Button
+                onClick={actions.onCancelClicked}
+                color="red"
+                size='medium'
+                floated="left">
+                Cancelar
+            </Button>
+            <Button.Group floated="right" size='medium' >
+                <Button style={{visibility: visibleOrBackButtons ? 'visible' : 'hidden'}} onClick={actions.onPreviousClicked}>Anterior</Button>
+                <Button.Or style={{visibility: visibleOrBackButtons ? 'visible' : 'hidden'}} />
+                <Button
+                    style={{visibility: visibleNextButton ? 'visible' : 'hidden'}}
+                    color="blue"
+                    onClick={actions.onNextClicked}>Siguiente</Button>
+            </Button.Group >
+        </Grid.Row >
+    );
+}
 
 export default WizardFooter;
